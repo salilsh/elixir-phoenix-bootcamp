0adeca6beed8959bd42be0d2fac2811d169237eb
salilsh <salil@hey.com>
2021-05-09 23:15:10 +0530

    updating project to the point where you can edit a specific topic

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -12,7 +12,7 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, _post} ->
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: topic_path(conn, :index))
@@ -27,5 +27,30 @@ defmodule Discuss.TopicController do
     topics = Repo.all(Topic)
 
     render conn, "index.html", topics: topics
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    changeset =
+      Topic
+      |> Repo.get(topic_id)
+      |> Topic.changeset(topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, _changeset} ->
+        conn
+        |> put_flash(:error, "Something went wrong")
+        |> redirect(to: topic_path(conn, :index))
+    end
   end
 end
